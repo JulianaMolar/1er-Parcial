93fb4add19f8c299ad48ddd7a5c726efefb3995e
Juliana Molar <a23328061310032@cetis131.edu.mx>
2025-02-13 13:22:00 -0600

    Add files via upload

---
+++ 4°A/juliana4A/hola4.php
@@ -0,0 +1,83 @@
+<DOCTYPE html>
+    <html lang="en">
+
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+            <link href="https://fonts.cdnfonts.com/css/healty" rel="stylesheet">
+  
+                  
+    
+            <title>Juliana Kali Molar Molar</title>
+        </head>
+        <body>
+            <nav class="navbar navbar-ligth" style="background-color: #003566;">
+                <div class="container">
+                    <a class="navbar-brand" href="#" style="color:#e0e1dd; font-family: 'UKIJ Chiwer Kesme', sans-serif;">Inicio</a>
+                    <!--boton de inicio que lleva a si mismo, de color blanco-->
+                    <!--a continuación es el menú drowdownd para poner las ligas a las prácticas -->
+                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarNavDropdownMenuLink"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="font-family: 'UKIJ Chiwer Kesme', sans-serif; color:#e0e1dd">Unidad 1</a>
+                                <!--Lo que sigue son los menos que se va a desplegar hacia abajo, cada uno tendra el nombre de su práctica, ejemplo, práctica 1 se llamara su nombre+el numero de la practica xx terminando con HTML-->
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <a class="dropdown-item" href="/juliana4A/hola1.php">Mostrar datos</a><br>
+                                    <a class="dropdown-item" href="/juliana4A/hola2.php">Meter datos</a><br>
+                                    <a class="dropdown-item" href="/juliana4A/hola3.php">datos</a><br>
+                                </div>
+                            </li>
+
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarNavDropdownMenuLink"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="font-family: 'UKIJ Chiwer Kesme', sans-serif; color:#e0e1dd">Unidad 2</a>
+                                <!--Lo que sigue son los menos que se va a desplegar hacia abajo, cada uno tendra el nombre de su práctica, ejemplo, práctica 1 se llamara su nombre+el numero de la practica xx terminando con HTML-->
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <a class="dropdown-item" href="/juliana4A/hola4.php">Práctica 4</a><br>
+                                    <a class="dropdown-item" href="#">Práctica 5</a><br>
+                                    <a class="dropdown-item" href="#">Práctica 6</a><br>
+                                </div>
+                            </li>
+
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarNavDropdownMenuLink"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="font-family: 'UKIJ Chiwer Kesme', sans-serif; color:#e0e1dd">Unidad 3</a>
+                                <!--Lo que sigue son los menos que se va a desplegar hacia abajo, cada uno tendra el nombre de su práctica, ejemplo, práctica 1 se llamara su nombre+el numero de la practica xx terminando con HTML-->
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <a class="dropdown-item" href="#">Práctica 7</a><br>
+                                    <a class="dropdown-item" href="#">Práctica 8</a><br>
+                                    <a class="dropdown-item" href="#">Práctica 9</a><br>
+                                </div>
+                            </li>
+                        
+                    </ul>
+                    </div>
+                </div></nav>
+            <div class="jumbotron">
+            <h1 class="display-4" style="text-align:center;font-family: 'Healty', sans-serif; color: #415a77;">miau</h1>
+                <p class="lead" style="text-align:center; font-size: medium;">Las Películas me gustan mucho, hay de muchos tipos y para muchos gustos.<br>
+                     En esta página expondré algunas de mis películas favoritas y el porque de esto. </p>
+                <hr class="my-4">
+                <p style="text-align:center; font-size: small;"><strong>Juliana Kali Molar Molar</strong></p>
+                <p class="lead"> 
+                </p>
+            </div>  
+            <div style="text-align: center;">
+                <img src="Img/hollywood.jpg" width="450" ><br></div>
+            <br><div class="row">
+                <div class="col-sm-2">
+                    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+                        <div class="card-header">Header</div>
+                        <div class="card-body">
+                          <h5 class="card-title">Primary card title</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+        </body>
+    </html>
